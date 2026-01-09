94050b46bf5129d29cf78f8f957f73dce9b430cf
oliveszn <ekpealamicheal@gmail.com>
2026-01-09 14:59:19 +0100

    did hero section

---
+++ client/components/Home/AboutSection.tsx
@@ -0,0 +1,12 @@
+export default function AboutSection() {
+  return (
+    <div>
+      Born on March 6, he spent most of his childhood in Ketu, Lagos, where he
+      developed a deep inclination towards music. From a young age, YKB
+      displayed a natural talent and passion for creating melodies, which
+      eventually led him to pursue a career in the music industry. YKB's music
+      can be described as a seamless fusion of Afrobeats, R&B, Trap, and
+      Hip-hop, showcasing his versatility as an artist.
+    </div>
+  );
+}
